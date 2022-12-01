fe57d74e78c384504d7161bd8f32b6371e778800
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2022-12-01 15:40:23 +0300

    GenericRepository oluşturuldu.

---
+++ DataAccessLayer/Concrete/GenericRepository.cs
@@ -0,0 +1,53 @@
+﻿using DataAccessLayer.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        Context context = new Context();
+
+        DbSet<T> _objects;
+
+        public GenericRepository()
+        {
+            _objects = context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _objects.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _objects.Update(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _objects.Remove(entity);
+            context.SaveChanges();
+        }
+
+        public List<T> GetAll(System.Linq.Expressions.Expression<Func<T, bool>> filter = null)
+        {
+            return filter == null
+                ? _objects.ToList()
+                : _objects.Where(filter).ToList();
+        }
+
+        public T Get(System.Linq.Expressions.Expression<Func<T, bool>> filter)
+        {
+            return _objects.SingleOrDefault(filter);
+        }
+    }
+}
+
