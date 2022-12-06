849a9996f0fa8ef41d6013874f2475561f419203
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2022-12-06 16:43:36 +0300

    Update ITypeDal.cs

--- DataAccessLayer/Abstract/ITypeDal.cs
+++ DataAccessLayer/Abstract/ITypeDal.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Type = EntityLayer.Concrete.Type;
 
 namespace DataAccessLayer.Abstract
 {
-    public interface ITypeDal:IGenericDal<Type>
+    public interface ITypeDal : IGenericDal<Type>
     {
     }
 }
