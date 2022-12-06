c1cffd3e754883cc0f3c0a880dd31d630cd3fab0
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2022-12-06 16:47:13 +0300

    Added Ef Dal classes

--- DataAccessLayer/Concrete/EntityFramework/EfTypeDal.cs
+++ DataAccessLayer/Concrete/EntityFramework/EfTypeDal.cs
@@ -5,10 +5,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Type = EntityLayer.Concrete.Type;
 
 namespace DataAccessLayer.Concrete.EntityFramework
 {
-    public class EfBackgroundDal : GenericRepository<Background>, IBackgroundDal
+    public class EfTypeDal : GenericRepository<Type>, ITypeDal
     {
     }
 }
