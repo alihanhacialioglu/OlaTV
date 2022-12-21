0801c507ea7f72bae16b059a1c8923b528712f32
Alihan Hacıalioğlu <alihanhacialioglu@gmail.com>
2022-12-21 18:40:17 +0300

    InvoiceDetailManager and IInvoiceDetailService were created.

---
+++ BusinessLayer/Managers/InvoiceDetailManager.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer.Services;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Manager
+{
+    public class InvoiceDetailManager : IInvoiceDetailService
+    {
+        IInvoiceDetailDal _invoiceDetailDal;
+        public InvoiceDetailManager(IInvoiceDetailDal invoiceDetailDal)
+        {
+            _invoiceDetailDal= invoiceDetailDal;
+        }
+        public void Add(InvoiceDetail invoiceDetail)
+        {
+            _invoiceDetailDal.Add(invoiceDetail);
+        }
+
+        public List<InvoiceDetail> GetAll()
+        {
+           return _invoiceDetailDal.GetAll();
+        }
+
+        public InvoiceDetail GetById(int id)
+        {
+            return _invoiceDetailDal.Get(g=>g.InvoiceDetailId==id);
+        }
+
+        public void Remove(InvoiceDetail invoiceDetail)
+        {
+            _invoiceDetailDal.Delete(invoiceDetail);
+        }
+
+        public void Update(InvoiceDetail invoiceDetail)
+        {
+            _invoiceDetailDal.Update(invoiceDetail);
+        }
+    }
+}
---
+++ BusinessLayer/Services/IInvoiceDetailService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public interface IInvoiceDetailService
+    {
+        void Add(InvoiceDetail invoiceDetail);
+        void Update(InvoiceDetail invoiceDetail);
+        void Remove(InvoiceDetail invoiceDetail);
+        List<InvoiceDetail> GetAll();
+        InvoiceDetail GetById(int id);
+    }
+}
