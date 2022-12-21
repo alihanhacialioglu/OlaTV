ce42e00c6c2a12821ed239e314a8babc7c197cff
Alihan Hacıalioğlu <alihanhacialioglu@gmail.com>
2022-12-21 18:28:21 +0300

    GenreManager and GenreService were created.

---
+++ BusinessLayer/Managers/GenreManager.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLayer.Services;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Manager
+{
+    public class GenreManager : IGenreService
+    {
+        IGenreDal _genreDal;
+        public GenreManager(IGenreDal genreDal)
+        {
+            _genreDal = genreDal;
+        }
+        public void Add(Genre genre)
+        {
+            _genreDal.Add(genre);
+        }
+
+        public List<Genre> GetAll()
+        {
+            return _genreDal.GetAll();
+        }
+
+        public Genre GetById(int id)
+        {
+            return _genreDal.Get(g => g.GenreId == id);
+        }
+
+        public Genre GetByName(string name)
+        {
+            return _genreDal.Get(g => g.GenreName == name);
+        }
+
+        public void Remove(Genre genre)
+        {
+            _genreDal.Delete(genre);
+        }
+
+        public void Update(Genre genre)
+        {
+            _genreDal.Update(genre);
+        }
+    }
+}
---
+++ BusinessLayer/Services/IGenreService.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public interface IGenreService
+    {
+        void Add(Genre genre);
+        void Remove(Genre genre);
+        void Update(Genre genre);
+        List<Genre> GetAll();
+        Genre GetById(int id);
+        Genre GetByName(string name);
+    }
+}
