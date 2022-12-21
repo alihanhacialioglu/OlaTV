61cd33e5ba510cf35e64650f5c78e6ade648ef0b
Alihan Hacıalioğlu <alihanhacialioglu@gmail.com>
2022-12-21 16:55:23 +0300

    CategoryManager and ICategoryService were created.

---
+++ BusinessLayer/Services/ICategoryService.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public interface ICategoryService
+    {
+        void Add(Category category);
+        void Update(Category category);
+        void Remove(Category category);
+        List<Category> GetAll();
+        Category GetById(int id);
+        Category GetByName(string name);
+    }
+}
