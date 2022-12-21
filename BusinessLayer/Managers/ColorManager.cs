59ec600d645acdfc7c7f004eb5024ccdd0be1526
Alihan Hacıalioğlu <alihanhacialioglu@gmail.com>
2022-12-21 17:22:46 +0300

    ColorManager and IColorService were created.

---
+++ BusinessLayer/Managers/ColorManager.cs
@@ -0,0 +1,50 @@
+﻿using BusinessLayer.Services;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Manager
+{
+    public class ColorManager : IColorService
+    {
+        IColorDal _colorDal;
+        
+        public ColorManager(IColorDal colorDal) 
+        {
+            _colorDal = colorDal;
+        } 
+        public void Add(Color color)
+        {
+            _colorDal.Add(color);
+        }
+
+        public List<Color> GetAll()
+        {
+           return _colorDal.GetAll();
+        }
+
+        public Color GetById(int id)
+        {
+            return _colorDal.Get(c=> c.ColorId== id);
+        }
+
+        public Color GetByName(string name)
+        {
+           return _colorDal.Get(c=>c.ColorName== name);
+        }
+
+        public void Remove(Color color)
+        {
+            _colorDal.Delete(color);
+        }
+
+        public void Update(Color color)
+        {
+            _colorDal.Update(color);
+        }
+    }
+}
---
+++ BusinessLayer/Services/IColorService.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public interface IColorService
+    {
+        void Add(Color color);
+        void Update(Color color);
+        void Remove(Color color);
+        List<Color> GetAll();
+        Color GetById(int id);
+        Color GetByName(string name);
+    }
+}
