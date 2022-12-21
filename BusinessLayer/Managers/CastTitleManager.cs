38fee6fcf394301f9c220fc563f2c89807b3f4ed
Alihan Hacıalioğlu <alihanhacialioglu@gmail.com>
2022-12-21 16:38:36 +0300

    CastTitleManager and ICastTitleService were created.

---
+++ BusinessLayer/Managers/CastTitleManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Services;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Manager
+{
+    public class CastTitleManager : ICastTitleService
+    {
+
+        ICastTitleDal _castTitleDal;
+        public CastTitleManager(ICastTitleDal castTitleDal) 
+        { 
+            _castTitleDal= castTitleDal;
+        }
+        public void Add(CastTitle castTitle)
+        {
+            _castTitleDal.Add(castTitle);
+        }
+
+        public List<CastTitle> GetAll()
+        {
+            return _castTitleDal.GetAll();
+        }
+
+        public CastTitle GetById(int id)
+        {
+            return _castTitleDal.Get(ct => ct.CastTitleId == id);
+        }
+
+        public void Remove(CastTitle castTitle)
+        {
+            _castTitleDal.Delete(castTitle);
+        }
+
+        public void Update(CastTitle castTitle)
+        {
+            
+            _castTitleDal.Update(castTitle);
+        }
+    }
+}
