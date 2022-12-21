3879be1a8a24ac3ded7af42b6d2c572c67600656
Alihan Hacıalioğlu <alihanhacialioglu@gmail.com>
2022-12-21 17:30:21 +0300

    CommunicationSettingManager and ICommunicationSettingService were created.

---
+++ BusinessLayer/Managers/CommunicationSettingManager.cs
@@ -0,0 +1,52 @@
+﻿using BusinessLayer.Services;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Manager
+{
+    public class CommunicationSettingManager : ICommunicationSettingService
+    {
+        ICommunicationSettingDal _communicationSettingDal;
+
+        public CommunicationSettingManager(ICommunicationSettingDal communicationSettingDal)
+        {
+            _communicationSettingDal = communicationSettingDal;
+        }
+
+        public void Add(CommunicationSetting communicationSetting)
+        {
+            _communicationSettingDal.Add(communicationSetting);
+        }
+
+        public List<CommunicationSetting> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CommunicationSetting GetById(int id)
+        {
+            return _communicationSettingDal.Get(cs => cs.CommunicationSettingId ==id);
+        }
+
+        public CommunicationSetting GetByName(string name)
+        {
+           return _communicationSettingDal.Get(cs => cs.CommunicationSettingName == name);
+        }
+
+        public void Remove(CommunicationSetting communicationSetting)
+        {
+            _communicationSettingDal.Delete(communicationSetting);
+        }
+
+        public void Update(CommunicationSetting communicationSetting)
+        {
+            _communicationSettingDal.Update(communicationSetting);
+        }
+    }
+
+}
---
+++ BusinessLayer/Services/ICommunicationSettingService.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public interface ICommunicationSettingService
+    {
+        void Add(CommunicationSetting communicationSetting);
+        void Update(CommunicationSetting communicationSetting);
+        void Remove(CommunicationSetting communicationSetting);
+        List<CommunicationSetting> GetAll();
+        CommunicationSetting GetById(int id);
+        CommunicationSetting GetByName(string name);
+
+    }
+}
