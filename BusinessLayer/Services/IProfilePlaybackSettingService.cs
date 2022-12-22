b81ba0f2865afdf069cb5faeeeda32fc19ff0336
Alihan Hacıalioğlu <alihanhacialioglu@gmail.com>
2022-12-22 23:38:12 +0300

    ProfilePlaybackSettingManager and IProfilePlaybackSettingService were updated.

---
+++ BusinessLayer/Services/IProfilePlaybackSettingService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public interface IProfilePlaybackSettingService
+    {
+        void Add(ProfilePlaybackSetting profilePlaybackSetting);
+        void Remove(ProfilePlaybackSetting profilePlaybackSetting);
+        void Update(ProfilePlaybackSetting profilePlaybackSetting);
+        List<ProfilePlaybackSetting> GetAll();
+        ProfilePlaybackSetting GetById(int id);
+    }
+}
