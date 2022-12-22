04e4e36515ba12f92947e870801cc43037e500a9
Alihan Hacıalioğlu <alihanhacialioglu@gmail.com>
2022-12-22 23:00:42 +0300

    PlaybackSettingManager and IPlaybackSettingService were created.

---
+++ BusinessLayer/Services/IPlaybackSettingService.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public interface IPlaybackSettingService
+    {
+        void Add(PlaybackSetting playback);
+        void Remove(PlaybackSetting playback);
+        void Update(PlaybackSetting playback);
+
+        List<PlaybackSetting> GetAll();
+        PlaybackSetting GetById(int id);
+        PlaybackSetting GetByName(string name);
+        PlaybackSetting GetByExplanation(string explanation);
+
+
+    }
+}
