b29bb3c761662005bc9961dbdd8a49617f9d4df7
Alihan Hacıalioğlu <alihanhacialioglu@gmail.com>
2022-12-23 11:43:45 +0300

    StyleContentManager and StyleContentService were created.

---
+++ BusinessLayer/Managers/StyleContentManager.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Services;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Manager
+{
+    public class StyleContentManager : IStyleContentService
+    {
+        IStyleContentDal _styleContentDal;
+        public StyleContentManager(IStyleContentDal styleContentDal)
+        {
+            _styleContentDal = styleContentDal;
+        }
+
+        public void Add(StyleContent styleContent)
+        {
+            _styleContentDal.Add(styleContent);
+        }
+
+        public List<StyleContent> GetAll()
+        {
+            return _styleContentDal.GetAll();
+        }
+
+        public StyleContent GetById(int id)
+        {
+           return  _styleContentDal.Get(s=>s.StyleContentId==id);
+        }
+
+        public void Remove(StyleContent styleContent)
+        {
+            _styleContentDal.Delete(styleContent);
+        }
+
+        public void Update(StyleContent styleContent)
+        {
+            _styleContentDal.Update(styleContent);
+        }
+    }
+}
---
+++ BusinessLayer/Services/IStyleContentService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public interface IStyleContentService
+    {
+        void Add(StyleContent styleContent);
+        void Remove(StyleContent styleContent);
+        void Update(StyleContent styleContent);
+        List<StyleContent> GetAll();
+        StyleContent GetById(int id);
+    }
+}
