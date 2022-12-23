d136303215b27da2748052bb8c3452c7283f514f
Alihan Hacıalioğlu <alihanhacialioglu@gmail.com>
2022-12-23 12:10:08 +0300

    TitleManager and ITitleService were created.

---
+++ BusinessLayer/Managers/TitleManager.cs
@@ -0,0 +1,50 @@
+﻿using BusinessLayer.Services;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Manager
+{
+    public class TitleManager : ITitleService
+    {
+        ITitleDal _titleDal;
+        public TitleManager(ITitleDal titleDal)
+        {
+            _titleDal = titleDal;
+        }
+
+        public void Add(Title title)
+        {
+            _titleDal.Add(title);
+        }
+
+        public List<Title> GetAll()
+        {
+            return _titleDal.GetAll();
+        }
+
+        public Title GetById(int id)
+        {
+            return _titleDal.Get(t => t.TitleId == id);
+        }
+
+        public Title GetByName(string name)
+        {
+            return _titleDal.Get(t=>t.TitleName== name);    
+        }
+
+        public void Remove(Title title)
+        {
+            _titleDal.Delete(title);
+        }
+
+        public void Update(Title title)
+        {
+            _titleDal.Update(title);
+        }
+    }
+}
---
+++ BusinessLayer/Services/ITitleService.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public interface ITitleService
+    {
+        void Add(Title title);
+        void Remove(Title title);
+        void Update(Title title);
+        List<Title> GetAll();
+        Title GetById(int id);
+        Title GetByName(string name);
+    }
+}
