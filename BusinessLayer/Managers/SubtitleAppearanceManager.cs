a43a2081685e80783149ff24017f9f27d8cdd507
Alihan Hacıalioğlu <alihanhacialioglu@gmail.com>
2022-12-23 11:58:18 +0300

    SubtitleAppearanceManager and ISubtitleAppearanceService were created.

---
+++ BusinessLayer/Managers/SubtitleAppearanceManager.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Services;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Manager
+{
+    public class SubtitleAppearanceManager : ISubtitleAppearanceService
+    {
+        ISubtitleAppearanceDal _subtitleAppearanceDal;
+        public SubtitleAppearanceManager(ISubtitleAppearanceDal subtitleAppearanceDal)
+        {
+            _subtitleAppearanceDal = subtitleAppearanceDal;
+        }
+
+        public void Add(SubtitleAppearance subtitleAppearance)
+        {
+            _subtitleAppearanceDal.Add(subtitleAppearance);
+        }
+
+        public List<SubtitleAppearance> GetAll()
+        {
+            return _subtitleAppearanceDal.GetAll();
+        }
+
+        public SubtitleAppearance GetById(int id)
+        {
+            return _subtitleAppearanceDal.Get(s => s.SubtitleAppearanceId == id);
+        }
+
+        public void Remove(SubtitleAppearance subtitleAppearance)
+        {
+            _subtitleAppearanceDal.Delete(subtitleAppearance);
+        }
+
+        public void Update(SubtitleAppearance subtitleAppearance)
+        {
+            _subtitleAppearanceDal.Update(subtitleAppearance);
+        }
+    }
+}
---
+++ BusinessLayer/Services/ISubtitleAppearanceService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public interface ISubtitleAppearanceService
+    {
+        void Add(SubtitleAppearance subtitleAppearance);
+        void Remove(SubtitleAppearance subtitleAppearance);
+        void Update(SubtitleAppearance subtitleAppearance);
+        List<SubtitleAppearance> GetAll();
+        SubtitleAppearance GetById(int id);
+    }
+}
