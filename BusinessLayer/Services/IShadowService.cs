0127886cc69e47317f45f6ba150fa9491c95d50d
Alihan Hacıalioğlu <alihanhacialioglu@gmail.com>
2022-12-23 11:38:25 +0300

    ShadowManager and IShadowService were created.

---
+++ BusinessLayer/Services/IShadowService.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public interface IShadowService
+    {
+        void Add(Shadow shadow);
+        void Remove(Shadow shadow);
+        void Update(Shadow shadow);
+        List<Shadow> GetAll();
+        Shadow GetById(int id);
+        Shadow GetByName(string name);
+    }
+}
