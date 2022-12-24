b1ca062558e4ca2dd312861bc8de7e9834d7fc33
Alihan Hacıalioğlu <alihanhacialioglu@gmail.com>
2022-12-24 16:34:29 +0300

    TypeContentManager and ITypeContentService were created.

---
+++ BusinessLayer/Managers/TypeContentManager.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Services;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Manager
+{
+    public class TypeContentManager : ITypeContentService
+    {
+        ITypeContentDal _typeContentDal;
+        public TypeContentManager(ITypeContentDal typeContentDal)
+        {
+            _typeContentDal = typeContentDal;
+        }
+
+        public void Add(TypeContent typeContent)
+        {
+            _typeContentDal.Add(typeContent);
+        }
+
+        public List<TypeContent> GetAll()
+        {
+            return _typeContentDal.GetAll();
+        }
+
+        public TypeContent GetById(int id)
+        {
+            return _typeContentDal.Get(t=>t.TypeContentId==id);
+        }
+
+        public void Remove(TypeContent typeContent)
+        {
+            _typeContentDal.Delete(typeContent);
+        }
+
+        public void Update(TypeContent typeContent)
+        {
+            _typeContentDal.Update(typeContent);
+        }
+    }
+}
---
+++ BusinessLayer/Services/ITypeContentService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public interface ITypeContentService
+    {
+        void Add(TypeContent typeContent);
+        void Remove(TypeContent typeContent);
+        void Update(TypeContent typeContent);
+        List<TypeContent> GetAll();
+        TypeContent GetById(int id);
+    }
+}
