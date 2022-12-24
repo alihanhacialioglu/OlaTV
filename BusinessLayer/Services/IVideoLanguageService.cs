dc677ceec70b2ece7ff525eccb1782e3c7275aca
Alihan Hacıalioğlu <alihanhacialioglu@gmail.com>
2022-12-24 16:36:33 +0300

    VideoLanguageManager and IVideoLanguageService were created.

---
+++ BusinessLayer/Services/IVideoLanguageService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public interface IVideoLanguageService
+    {
+        void Add(VideoLanguage videoLanguage);
+        void Remove(VideoLanguage videoLanguage);
+        void Update(VideoLanguage videoLanguage);
+        List<VideoLanguage> GetAll();
+        VideoLanguage GetById(int id);
+    }
+}
