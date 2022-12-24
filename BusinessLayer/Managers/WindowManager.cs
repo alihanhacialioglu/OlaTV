36f0b2dbd1fbebeac99d35df15f6b3f4d45c8451
Alihan Hacıalioğlu <alihanhacialioglu@gmail.com>
2022-12-24 16:38:02 +0300

    WindowManager and IWindowService were created.

---
+++ BusinessLayer/Managers/WindowManager.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer.Services;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Manager
+{
+    public class WindowManager : IWindowService
+    {
+        IWindowDal _windowDal;
+        public WindowManager(IWindowDal windowDal)
+        {
+            _windowDal = windowDal;
+        }
+        public void Add(Window window)
+        {
+            _windowDal.Add(window);
+        }
+
+        public List<Window> GetAll()
+        {
+            return _windowDal.GetAll();
+        }
+
+        public Window GetById(int id)
+        {
+            return _windowDal.Get(w => w.WindowId == id);
+        }
+
+        public void Remove(Window window)
+        {
+            _windowDal.Delete(window);
+        }
+
+        public void Update(Window window)
+        {
+            _windowDal.Update(window);
+        }
+    }
+}
---
+++ BusinessLayer/Services/IWindowService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public interface IWindowService
+    {
+        void Add(Window window);
+        void Remove(Window window);
+        void Update(Window window);
+        List<Window> GetAll();
+        Window GetById(int id);
+    }
+}
