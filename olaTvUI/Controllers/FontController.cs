c8615d0ee26bb1a209f4d47145c598f4318c3d6f
Alihan Hacıalioğlu <alihanhacialioglu@gmail.com>
2023-01-06 00:26:00 +0300

    FontController was created.

--- olaTvUI/Controllers/FontController.cs
+++ olaTvUI/Controllers/FontController.cs
@@ -1,7 +1,9 @@
 ﻿using BusinessLayer.Manager;
 using DataAccessLayer.Concrete.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
 
 namespace OlaTvUI.Controllers
 {
@@ -14,6 +16,37 @@ namespace OlaTvUI.Controllers
             return View(fonts);
         }
 
-       
+        [HttpGet]
+        public IActionResult Font_Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Font_Add(EntityLayer.Concrete.Font font)
+        {
+            fontManager.Add(font);
+            return RedirectToAction("Font_Index");
+        }
+
+        public IActionResult Font_Update(int id)
+        {
+            EntityLayer.Concrete.Font font = fontManager.GetById(id);
+            return View(font);
+        }
+
+        [HttpPost]
+        public IActionResult Font_Update(EntityLayer.Concrete.Font font)
+        {
+            fontManager.Update(font);
+            return RedirectToAction("Font_Index");
+        }
+
+        public IActionResult Font_Delete(int id)
+        {
+            EntityLayer.Concrete.Font font = fontManager.GetById(id);
+            font.IsDelete = true;
+            fontManager.Update(font);
+            return View("Font_Index");
+        }
     }
 }
