e2bbdb37176c0bb350a38ad663241773466a2ff1
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2023-01-05 18:43:46 +0300

    Added Shadow

---
+++ OlaTvUI/Models/ShadowColorModel.cs
@@ -0,0 +1,10 @@
+﻿using EntityLayer.Concrete;
+
+namespace OlaTvUI.Models
+{
+    public class ShadowColorModel
+    {
+        public Shadow ShadowModal { get; set;}
+        public IEnumerable<Color> Colors { get; set; }
+    }
+}
