13105c199e5118f2f625e7b3adb285d5fdaf490a
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2023-01-06 16:06:23 +0300

    Renamed the model

---
+++ OlaTvUI/Models/ShadowModel.cs
@@ -0,0 +1,11 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace OlaTvUI.Models
+{
+    public class ShadowModel
+    {
+        public Shadow? Shadow { get; set;}
+        public IEnumerable<Color>? Colors { get; set; }
+    }
+}
