10988d4d0e4b56b60c7fe0ffd204719dcd6db5ec
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2023-01-10 13:48:08 +0300

    Added MaturityRatingValidator

---
+++ BusinessLayer/Validations/MaturityRatingValidator.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Validations
+{
+	public class MaturityRatingValidator : AbstractValidator<MaturityRating>
+	{
+		public MaturityRatingValidator()
+		{
+			RuleFor(m => m.StatusName).NotEmpty();
+			RuleFor(m => m.MaturityExplanation).NotEmpty();
+		}
+	}
+}
