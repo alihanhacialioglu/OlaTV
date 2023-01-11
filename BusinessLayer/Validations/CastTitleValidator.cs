01cfe4422c03b85dc91b7ff3000ab0f68b90114c
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2023-01-11 12:31:49 +0300

    Added CastTitleValidator

---
+++ BusinessLayer/Validations/CastTitleValidator.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Validations
+{
+    public class CastTitleValidator : AbstractValidator<CastTitle>
+    {
+        public CastTitleValidator()
+        {
+            RuleFor(m=>m.CastId).NotEmpty();
+            RuleFor(m=>m.TitleId).NotEmpty();
+        }
+    }
+}
