1d7c0831f3f59c9ea19a78f2f83fe3d9683702f1
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2023-01-11 15:57:07 +0300

    Added GenreContentValidator

---
+++ BusinessLayer/Validations/GenreContentValidator.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Validations
+{
+	public class GenreContentValidator : AbstractValidator<GenreContent>
+	{
+		public GenreContentValidator()
+		{
+			RuleFor(m => m.Genre).NotEmpty();
+			RuleFor(m => m.Content).NotEmpty();
+		}
+	}
+}
