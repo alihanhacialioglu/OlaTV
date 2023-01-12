903da48c9421b5534841345966bece2a02c5d7b7
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2023-01-12 17:04:29 +0300

    Added ContentValidator

---
+++ BusinessLayer/Validations/ContentValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Validations
+{
+	public class ContentValidator : AbstractValidator<Content>
+	{
+		public ContentValidator()
+		{
+			RuleFor(m => m.ContentName).NotEmpty();
+			RuleFor(m => m.ContentName).MaximumLength(25);
+
+			RuleFor(m => m.MaturityRatingId).NotEmpty();
+		}
+	}
+}
