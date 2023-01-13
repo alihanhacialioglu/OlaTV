43e3de019c37629f36ab7f3a61386795d412c15c
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2023-01-13 10:46:34 +0300

    Added ContentCastTitleValidator

---
+++ BusinessLayer/Validations/ContentCastTitleValidator.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Validations
+{
+	public class ContentCastTitleValidator : AbstractValidator<ContentCastTitle>
+	{
+		public ContentCastTitleValidator()
+		{
+			RuleFor(m=>m.ContentId).NotEmpty();
+			RuleFor(m=>m.CastTitleId).NotEmpty();
+		}
+	}
+}
