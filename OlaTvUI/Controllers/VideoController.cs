aed0430a7b8f676807834b2d59935dd4e9ad0ada
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2023-01-14 11:15:35 +0300

    Added Video

---
+++ OlaTvUI/Controllers/VideoController.cs
@@ -0,0 +1,118 @@
+﻿using BusinessLayer.Manager;
+using BusinessLayer.Validations;
+using DataAccessLayer.Concrete.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using OlaTvUI.Models;
+
+namespace OlaTvUI.Controllers
+{
+    public class VideoController : Controller
+    {
+        VideoManager videoManager = new VideoManager(new EfVideoDal());
+        LanguageManager languageManager = new LanguageManager(new EfLanguageDal());
+        ContentManager contentManager = new ContentManager(new EfContentDal());
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+        public IActionResult Video_Index()
+        {
+            var videos = videoManager.GetAll();
+            return View(videos);
+        }
+        [HttpGet]
+        public IActionResult Video_Add()
+        {
+            VideoModel videoModel = new VideoModel();
+            videoModel.Video = new Video();
+            videoModel.Languages = languageManager.GetAll();
+            videoModel.Contents = contentManager.GetAll();
+            videoModel.Categories = categoryManager.GetAll();
+            return View(videoModel);
+        }
+        [HttpPost]
+        public IActionResult Video_Add(Video video)
+        {
+            VideoModel videoModel = new VideoModel();
+            videoModel.Video = video;
+            videoModel.Languages = languageManager.GetAll();
+            videoModel.Contents = contentManager.GetAll();
+            videoModel.Categories = categoryManager.GetAll();
+            VideoValidator validator = new VideoValidator();
+            var result = validator.Validate(video);
+            if (result.IsValid)
+            {
+                videoManager.Add(video);
+                return RedirectToAction("Video_Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View(videoModel);
+            }
+        }
+
+        public IActionResult Video_Update(int id)
+        {
+            Video video = videoManager.GetById(id);
+            VideoModel videoModel = new VideoModel();
+            videoModel.Video = video;
+            videoModel.Languages = languageManager.GetAll();
+            videoModel.Contents = contentManager.GetAll();
+            videoModel.Categories = categoryManager.GetAll();
+            return View(videoModel);
+        }
+
+        [HttpPost]
+        public IActionResult Video_Update(Video video)
+        {
+            VideoModel videoModel = new VideoModel();
+            videoModel.Video = video;
+            videoModel.Languages = languageManager.GetAll();
+            videoModel.Contents = contentManager.GetAll();
+            videoModel.Categories = categoryManager.GetAll();
+
+            VideoValidator validator = new VideoValidator();
+            var result = validator.Validate(video);
+
+            if (result.IsValid)
+            {
+                videoManager.Update(video);
+                return RedirectToAction("Video_Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View(videoModel);
+            }
+        }
+
+        public IActionResult Video_Activate(int id)
+        {
+            Video video = videoManager.GetById(id);
+            video.IsDelete = false;
+            videoManager.Update(video);
+            return RedirectToAction("Video_Index");
+        }
+
+        public IActionResult Video_Deactivate(int id)
+        {
+            Video video = videoManager.GetById(id);
+            video.IsDelete = true;
+            videoManager.Update(video);
+            return RedirectToAction("Video_Index");
+        }
+
+        public IActionResult Video_Delete(int id)
+        {
+            Video video = videoManager.GetById(id);
+            videoManager.Remove(video);
+            return RedirectToAction("Video_Index");
+        }
+    }
+}
---
+++ OlaTvUI/Models/VideoModel.cs
@@ -0,0 +1,12 @@
+﻿using EntityLayer.Concrete;
+
+namespace OlaTvUI.Models
+{
+    public class VideoModel
+    {
+        public Video Video { get; set; }
+        public IEnumerable<Language> Languages { get; set; }
+        public IEnumerable<Content> Contents { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
