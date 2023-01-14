16c55335bcdc60a80f7b8d8af2a99062322bca21
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2023-01-14 14:43:06 +0300

    Added VideoLanguageValidator

---
+++ BusinessLayer/Validations/VideoLanguageValidator.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Validations
+{
+	public class VideoLanguageValidator : AbstractValidator<VideoLanguage>
+	{
+		public VideoLanguageValidator()
+		{
+			RuleFor(m => m.VideoId).NotEmpty().WithMessage("Video cannot be empty");
+			RuleFor(m => m.LanguageId).NotEmpty().WithMessage("Subtitle Language cannot be empty");
+		}
+	}
+}
