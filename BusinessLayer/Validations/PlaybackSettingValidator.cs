3adaa183ad1504b5d9576cdb3993a2b4224e921d
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2023-01-15 12:58:30 +0300

    Added PlaybackSetting

---
+++ BusinessLayer/Validations/PlaybackSettingValidator.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Validations
+{
+    public class PlaybackSettingValidator : AbstractValidator<PlaybackSetting>
+    {
+        public PlaybackSettingValidator()
+        {
+            RuleFor(m=>m.PlaybackSettingName).NotEmpty().WithMessage("This field cannot be left blank");
+            RuleFor(m=>m.PlaybackSettingExplanation).NotEmpty().WithMessage("This field cannot be left blank");
+        }
+    }
+}
---
+++ OlaTvUI/Controllers/PlaybackSettingController.cs
@@ -0,0 +1,98 @@
+﻿using BusinessLayer.Manager;
+using BusinessLayer.Validations;
+using DataAccessLayer.Concrete.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OlaTvUI.Controllers
+{
+    public class PlaybackSettingController : Controller
+    {
+        PlaybackSettingManager playbackSettingManager = new PlaybackSettingManager(new EfPlaybackSettingDal());
+
+		public IActionResult PlaybackSetting_Index()
+		{
+			var playbackSettings = playbackSettingManager.GetAll();
+			return View(playbackSettings);
+		}
+
+		[HttpGet]
+		public IActionResult PlaybackSetting_Add()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult PlaybackSetting_Add(PlaybackSetting playbackSetting)
+		{
+			PlaybackSettingValidator validator = new PlaybackSettingValidator();
+			var result = validator.Validate(playbackSetting);
+
+			if (result.IsValid)
+			{
+				playbackSettingManager.Add(playbackSetting);
+				return RedirectToAction("PlaybackSetting_Index");
+			}
+			else
+			{
+				foreach (var item in result.Errors)
+				{
+					ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+				}
+				return View();
+			}
+		}
+
+		[HttpGet]
+		public IActionResult PlaybackSetting_Update(int id)
+		{
+			PlaybackSetting playbackSetting = playbackSettingManager.GetById(id);
+			return View(playbackSetting);
+		}
+
+		[HttpPost]
+		public IActionResult PlaybackSetting_Update(PlaybackSetting playbackSetting)
+		{
+			PlaybackSettingValidator validator = new PlaybackSettingValidator();
+			var result = validator.Validate(playbackSetting);
+
+			if (result.IsValid)
+			{
+				playbackSettingManager.Update(playbackSetting);
+				return RedirectToAction("PlaybackSetting_Index");
+			}
+			else
+			{
+				foreach (var item in result.Errors)
+				{
+					ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+				}
+				return View();
+			}
+		}
+
+		public IActionResult PlaybackSetting_Delete(int id)
+		{
+			PlaybackSetting textsize = playbackSettingManager.GetById(id);
+			playbackSettingManager.Remove(textsize);
+			return RedirectToAction("PlaybackSetting_Index");
+
+		}
+
+		public IActionResult PlaybackSetting_Activate(int id)
+		{
+			PlaybackSetting playbackSetting = playbackSettingManager.GetById(id);
+			playbackSetting.IsDelete = false;
+			playbackSettingManager.Update(playbackSetting);
+			return RedirectToAction("PlaybackSetting_Index");
+		}
+
+		public IActionResult PlaybackSetting_Deactivate(int id)
+		{
+			PlaybackSetting playbackSetting = playbackSettingManager.GetById(id);
+			playbackSetting.IsDelete = true;
+			playbackSettingManager.Update(playbackSetting);
+			return RedirectToAction("PlaybackSetting_Index");
+		}
+    }
+}
